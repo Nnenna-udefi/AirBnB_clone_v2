1bc56cce9759123f9df6cdc68973f180129dd988
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-20 08:16:22 -0700

    console

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] =='}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -138,7 +138,7 @@ class HBNBCommand(cmd.Cmd):
                     # Integer value
                     value = int(value)
                 params[key] = value
-            except:
+            except SyntaxError:
                 pass
                 # Skip unrecognized parameters
         # Create a new instance of the class with the given parameters
@@ -339,6 +339,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
