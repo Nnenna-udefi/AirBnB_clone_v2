d229294e516e7fa822027a63984d21feee1c1446
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-04 14:34:11 -0700

    deploy

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""script that creates and distributes the function deploy to the web servers
+"""
+from fabric.api import *
+import os.path
+from datetime import datetime
+
+env.hosts = ['34.203.75.215', '54.175.88.234']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a .tgz from html folder"""
+    # creates the versions directory
+    local('mkdir -p versions')
+
+    dnow = datetime.now()
+    archive_name = 'web_static_{}{}{}{}{}{}.tgz'.format(dnow.year, dnow.month,
+                                                        dnow.day, dnow.hour,
+                                                        dnow.minute,
+                                                        dnow.second)
+    # create archive and store it in the version directory
+    arch_store = local("tar -cvzf versions/{} web_static".format(archive_name))
+
+    if arch_store.succeeded:
+        return 'versions/{}'.format(archive_name)
+    else:
+        return None
+
+def do_deploy(archive_path):
+    """deploys and distributes archive"""
+    if os.path.isfile(archive_path) is False:
+        return False
+
+    put(archive_path, '/tmp/')
+
+    # Extract the archive to the new folder on the server
+    filename = archive_path.split('/')[-1]
+    foldername = '/data/web_static/releases/' + filename.split('.')[0]
+    run('mkdir -p {}'.format(foldername))
+    run('tar -xzf /tmp/{} -C {} --strip-components=1'.format(filename, foldername))
+
+    # Remove the archive from the server
+    run('rm /tmp/{}'.format(filename))
+
+    # Move the contents of the web_static folder to the new folder
+    run('mv {}/web_static/* {}/'.format(foldername, foldername))
+    run('rm -rf {}/web_static'.format(foldername))
+
+    # Delete the symbolic link to the current version and create a new one
+    run('rm -f /data/web_static/current')
+    run('ln -s {} /data/web_static/current'.format(foldername))
+
+    return True
+
+def deploy():
+    """call do_pack and do_deploy functions"""
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
