aa6818c391101bb07204a565451416066b86b175
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-04 12:16:32 -0700

    fabricpy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,14 +14,13 @@ def do_pack():
 
     dnow = datetime.now()
     archive_name = 'web_static_{}{}{}{}{}{}.tgz'.format(dnow.year, dnow.month,
-                                                       dnow.day, dnow.hour,
-                                                       dnow.minute,
-                                                       dnow.second)
-    
+                                                        dnow.day, dnow.hour,
+                                                        dnow.minute,
+                                                        dnow.second)
     # create archive and store it in the version directory
-    arch_storage = local("tar cvzf version/{} /web_static".format(archive_name))
+    arch_store = local("tar cvzf versions/{} /web_static".format(archive_name))
 
-    if arch_storage.succeeded:
+    if arch_store.succeeded:
         return 'versions/{}'.format(archive_name)
     else:
         return None
