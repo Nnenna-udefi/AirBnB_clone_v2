2cf88356f441ff366e460114adada9ebc46e66a6
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-27 09:21:00 -0700

    states

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
     """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
     return render_template("10-hbnb_filters.html",
                            states=states, amenities=amenities)
 
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+"""
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """display a HTML page: (inside the tag BODY)"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy session."""
+    """closes the storage on teardown"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
